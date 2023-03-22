5c453c9358331d7f6cb938859a7c1923c0fd3406
Jeeva J <jeevaj@Jeevas-MacBook-Air.local>
2023-03-22 19:52:06 -0400

    Finished pseudocode and review of 1 part

--- harvest.py
+++ harvest.py
@@ -10,7 +10,7 @@ class MelonType:
         self, code, first_harvest, color, is_seedless, is_bestseller, name
     ):
         """Initialize a melon."""
-
+    # initializing self to every attribute
         self.pairings = []
         self.code = code
         self.first_harvest = first_harvest
@@ -19,17 +19,16 @@ class MelonType:
         self.is_bestseller = is_bestseller
         self.name = name
 
-        # Fill in the rest
-
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
+    #  this is where the pairing argument that is passed through add_pairing(mint) that gets appended to self.pairings on line14"""
 
-        # Fill in the rest
+        self.pairings.append(pairing)
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
-
-        # Fill in the rest
+    # every time .update_code is used the argument passed in .update_code() will be placed into self.code found in the __init__
+        self.code = new_code
 
 
 def make_melon_types():
